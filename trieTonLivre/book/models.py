8d572a3b72f34e8e39ece0682b019cb9aa841210
Guillaume SIMON <guillaumesimon597@gmail.com>
2025-02-05 15:03:23 +0100

    Implement Book and Author models with serializers and views; add Celery tasks for book management

--- trieTonLivre/book/models.py
+++ trieTonLivre/book/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+    ids = models.AutoField(primary_key=True)
+    idGutendex = models.IntegerField()
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    publication_date = models.DateField()
+    author = models.ManyToManyField('Author')
     summary = models.TextField()
-    # cover = models.ImageField(upload_to='covers/')
+    cover = models.URLField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    linkToBook = models.URLField(max_length=200)
+    downloadCount = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.title
+
+class Author(models.Model):
+    idsAuthor = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    birth_date = models.DateField(null=True, blank=True)
+    death_date = models.DateField(null=True, blank=True)
