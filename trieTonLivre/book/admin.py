203fba544818ec275dd45e8ae34ea64557932a4d
Guillaume SIMON <guillaumesimon597@gmail.com>
2025-02-06 15:13:41 +0100

    Add search by index

--- trieTonLivre/book/admin.py
+++ trieTonLivre/book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Author, Book, WordOccurrence
+
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(WordOccurrence)
+admin.site.register(Author)
--- trieTonLivre/book/urls.py
+++ trieTonLivre/book/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import BookViewSet
+from .views import BookViewSet,request_book,WordOccurency
 
 urlpatterns = [
     path('', BookViewSet.as_view({'get': 'list'})),
+    path('occurence', WordOccurency.as_view({'get': 'list'})),
+    path('search', WordOccurency.as_view({'get': 'search'})),
+    path("request",view=request_book),
 ]
